4edc27df3dedc42b4cd7f419ab1d05531bbc650a
gizmore <gizmore@wechall.net>
2026-09-18 04:04:18 +0200

    'more korean'

---
+++ lang/invite_ko.php
@@ -0,0 +1,11 @@
+<?php
+return [
+	'link_invite' => '누군가를 초대하세요',
+	'cfg_invite_max_pending' => '사용자당 최대 보류 중인 초대 수입니다. -1이면 제한이 없습니다.',
+	'mt_invite_form' => '누군가를 초대하세요',
+	'err_invite_max_pendings' => '대기 중인 최대 초대 수 %s개를 초과했습니다.',
+	'err_invite_already_member' => '이 사람은 이미 이 사이트의 회원입니다.',
+	'err_already_invited' => '이 사람은 이미 초대되었습니다.',
+	'msg_invited' => '이 주소로 초대 메일을 보냈습니다.',
+	'invite_mail_subj' => '[%s] 초대',
+];
